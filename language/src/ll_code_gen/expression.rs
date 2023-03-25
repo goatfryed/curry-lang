618a98441ffbd2f1963fc7329b474f9c296a1618
Omar Goatfryed Sood <goatfryed@gmail.com>
2023-03-25 22:21:12 +0100

    feat: introduce simple strings and variable expressions

--- language/src/ll_code_gen/expression.rs
+++ language/src/ll_code_gen/expression.rs
@@ -1,5 +1,5 @@
 use anyhow::*;
-use inkwell::values::{BasicMetadataValueEnum, BasicValue};
+use inkwell::values::{BasicMetadataValueEnum, BasicValue, IntValue, PointerValue};
 use pest::iterators::Pair;
 use crate::ll_code_gen::function_generator::FunctionGenerator;
 use crate::parser::curry_pest::{Rule, PairHelper};
@@ -16,7 +16,10 @@ pub fn generate_expression<'gen>(pair: Pair<Rule>, scope: &FunctionGenerator<'ge
                 it => todo!("build_fn_arg value - {}", it)
             }
         },
-        it => todo!("build_fn_arg {}", it)
+        _ => {
+            let variable_expr: VariableValue = pair.into();
+            variable_expr.resolve_variable(scope)
+        }
     }
 }
 
@@ -26,11 +29,17 @@ struct VariableValue<'i> {
 }
 impl <'i> VariableValue<'i> {
     fn resolve_variable<'gen>(&self, scope: &FunctionGenerator<'gen,'_,'_>) -> Result<BasicMetadataValueEnum<'gen>> {
+        let context = scope.parent.parent.context;
         let variable_name = self.pair.as_str();
         let symbols = scope.symbols.borrow();
-        let variable = symbols.get(variable_name)
+        let string_pointer = *symbols.get(variable_name)
             .with_context(|| format!("usage of unknown variable '{}'", variable_name))?;
-        Ok(BasicMetadataValueEnum::from(variable.as_basic_value_enum()))
+        let i8_type = context.i8_type();
+        let indices = [i8_type.const_zero(), i8_type.const_zero()];
+        unsafe {
+            let pointer_value = scope.builder.build_in_bounds_gep(string_pointer, &indices, "tmp");
+            Ok(BasicMetadataValueEnum::from(pointer_value.as_basic_value_enum()))
+        }
     }
 }
 
@@ -39,11 +48,29 @@ struct StringConstant<'i> {
     pair: Pair<'i,Rule>
 }
 impl <'i> StringConstant<'i> {
-    fn generate<'gen>(mut self, scope: &FunctionGenerator<'gen,'_,'_>) -> Result<BasicMetadataValueEnum<'gen>> {
+    fn generate<'gen>(self, scope: &FunctionGenerator<'gen,'_,'_>) -> Result<BasicMetadataValueEnum<'gen>> {
         let pair = self.pair.unique_inner().context("access string_value")?;
-        let value = pair.as_str();
-        // let string_ref = scope.parent.parent.context.const_string(value.as_bytes(),true);
-        let string_ref = scope.builder.build_global_string_ptr(value, "string");
-        Ok(BasicMetadataValueEnum::from(string_ref.as_basic_value_enum()))
+        let raw_string = pair.as_str();
+        let value = Self::decode_user_string(raw_string);
+        let context = scope.parent.parent.context;
+        let string_value = context.const_string(value.as_bytes(), true);
+        let string_pointer = scope.builder.build_alloca(string_value.get_type(), "str_val");
+        scope.builder.build_store(string_pointer, string_value);
+        let i8_type = context.i8_type();
+        let indices = [i8_type.const_zero(), i8_type.const_zero()];
+        unsafe {
+            let pointer_value = scope.builder.build_in_bounds_gep(string_pointer, &indices, "tmp");
+            Ok(BasicMetadataValueEnum::from(pointer_value.as_basic_value_enum()))
+        }
+    }
+
+    fn decode_user_string(raw_string: &str) -> String {
+        raw_string.to_string()
+            .replace("\\\"", "\"")
+            .replace("\\\\", "\\")
+            .replace("\\n", "\n")
+            .replace("\\r", "\r")
+            .replace("\\t", "\t")
+            .replace("\\0", "\0")
     }
 }
